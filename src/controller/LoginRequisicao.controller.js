93e8decd061f823a61dc9dab8d586d513b8cca86
gabriel <gabrielmineironogueira@gmail.com>
2022-07-12 19:04:37 -0300

    feat: finalizando página de login

---
+++ src/controller/LoginRequisicao.controller.js
@@ -0,0 +1,22 @@
+class RequisicaoLogin{
+    static url = "https://habits-kenzie.herokuapp.com/api/userLogin"
+    static async login(loginData){
+        const response = await fetch(this.url,{
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(loginData)
+        })
+
+        const data = await response.json()
+        window.localStorage.setItem("username", data.response.usr_name)
+        window.localStorage.setItem("email", data.response.usr_email)
+        window.localStorage.setItem("image", data.response.usr_image)
+        window.localStorage.setItem("token", data.token)
+        console.log(data)
+        return data
+    }
+
+}
+export default RequisicaoLogin
