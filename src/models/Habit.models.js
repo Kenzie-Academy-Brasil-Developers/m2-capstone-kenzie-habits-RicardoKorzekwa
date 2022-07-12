9e8198176a5812e081c160fd9327a53a4b91ad12
ricardokorzekwa <korzekwa.ricardo@gmail.com>
2022-07-12 13:47:30 -0300

    Endpoint Habitos criado

--- src/models/Habit.models.js
+++ src/models/Habit.models.js
@@ -1,3 +1,7 @@
 export default class Habit{
-    
+    static UrlBase = "https://habits-kenzie.herokuapp.com/api/habits"
+    static async criarHabito(habito){
+        return await fetch()
+
+    }
 }
