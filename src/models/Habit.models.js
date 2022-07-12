2e51ba6485c784ce643baae9fc7df5ff75c59c2c
ricardokorzekwa <korzekwa.ricardo@gmail.com>
2022-07-12 14:17:49 -0300

    Endpoint Habitos

--- src/models/Habit.models.js
+++ src/models/Habit.models.js
@@ -1,7 +1,81 @@
 export default class Habit{
-    static UrlBase = "https://habits-kenzie.herokuapp.com/api/habits"
+    static urlBase  = "https://habits-kenzie.herokuapp.com/api/habits"
+    static token    = JSON.parse(localStorage.getItem("@kenzie-habit:token"))
+    
     static async criarHabito(habito){
-        return await fetch()
-
+        return await fetch(this.urlBase, {
+            method  : "POST",
+            headers : {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${token}`
+            },
+            body: JSON.stringify(habito)
+        })
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.log(err))
     }
+
+    static async listarTodosHabitos(){
+        return await fetch(this.urlBase, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.log(err))
+    }
+
+    static async listarHabitosXcategoria(categoria){
+        return await fetch(`${this.urlBase}/category/:habit_${categoria}`, {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.log(err))
+    }
+
+    static async atualizarHabito(id, habitoAtualizado){
+        return await fetch(`${this.urlBase}/complete/:habit_${id}`,{
+            method: "PATCH",
+            headers: {
+                "Content-Type"  : "application/json",
+                Authorization   : `Bearer ${token}`
+            },
+            body: JSON.stringify(habitoAtualizado)
+        })
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.log(err))
+    }
+
+    static async completarHabito(id){
+        return await fetch(`${this.urlBase}/complete/:habit_${id}`,{
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.log(err))
+    }
+
+    static async deleteHabito(id){
+        return await fetch(`${this.urlBase}/complete/:habit_${id}`,{
+            method: "DELETE",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        })
+        .then(res => res.json())
+        .then(res => res)
+        .catch(err => console.log(err))
+    }
+
 }
